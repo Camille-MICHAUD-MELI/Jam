4f00f6d3562e86757be9bfb3380d5180b0dd987b
Camille-MICHAUD-MELI <72015249+Camille-MICHAUD-MELI@users.noreply.github.com>
2021-06-12 14:55:19 +0200

    push

---
+++ Assets/Scripts/Move.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+public class Move : MonoBehaviour
+{
+
+    public float speed = 0.18f;
+    public float jumph = 4.01f;
+
+    void Start() { print("Start"); }
+
+    void Update()
+    {
+
+        Vector3 dp = new Vector3();
+
+        if (Input.GetKey(KeyCode.Q))
+        {
+            dp.x -= speed;
+        }
+
+        if (Input.GetKey(KeyCode.D))
+        {
+            dp.x += speed;
+        }
+
+        transform.position += dp;
+    }
+}
